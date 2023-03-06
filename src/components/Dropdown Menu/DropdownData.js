12ee001ef9b3e8531af5bf5663e5136fb5b4afdf
SamuelS45 <104377311+SamuelS45@users.noreply.github.com>
2023-03-07 00:29:21 +0700

    Update DropdownData.js

--- src/components/Dropdown Menu/DropdownData.js
+++ src/components/Dropdown Menu/DropdownData.js
@@ -2,24 +2,24 @@ import React from "react"
 
 const DropdownData = [
     {
-        title: 'Techniques in Farming',
-        text:'Agriculture or farming is the practice of cultivating plants and livestock. Agriculture was the key development in the rise of sedentary human civilization, whereby farming of domesticated species created food surpluses that enabled people to live in cities. The history of agriculture began thousands of years ago.'
+        title: ' What is Arabica Coffee?',
+        text:'Arabica coffee is a type of coffee plant that produces high-quality beans with rich flavor and aroma. It is the most widely cultivated coffee species in the world1. Arabica coffee originated in Ethiopia and Yemen, but now grows in many regions with mild climates and high altitudes.'
     },
     {
-        title: 'How is organic farming better for the environment than conventional farming?',
-        text:'Critics say organic farming is less efficient than conventional farming, and so uses more land, leading to greater deforestation, which causes higher carbon dioxide emissions and biodiversity loss.'
+        title: 'Where did coffee come from?',
+        text:'The origin of coffee is not very clear, but there are some legends and historical records that suggest it came from Ethiopia or Yemen'
     },
     {
-        title: 'Is organic no-till farming possible?',
-        text:'No-till farming (also known as zero tillage or direct drilling) is an agricultural technique for growing crops or pasture without disturbing the soil through tillage. No-till farming decreases the amount of soil erosion tillage causes in certain soils, especially in sandy and dry soils on sloping terrain.'
+        title: 'What are some health benifits of Coffee',
+        text:'Coffee has some potential health benefits, such as boosting energy levels, lowering the risk of some diseases, and protecting against oxidative stress.'
     },
     {
-        title: "If you can't overwater plants in soil, why don't plants drown in hydroponics?",
-        text:'Properly done hydroponic systems do not drown or overwater plants because the root system is never completely submerged in water. In addition, hydroponic systems often employ air pumps and air stones to oxygenate the water, providing oxygen to the submerged root system that otherwise will drown.'
+        title: "How big is the coffee industry?",
+        text:'The coffee industry is a large and growing sector that involves production, processing, trade, and consumption of coffee. According to some estimates, the global coffee market size was valued at USD 79.5 billion in 2022 and is projected to reach USD 112.8 billion by 2030.'
     },
     {
-        title: 'Is farming profitable?',
-        text:'Although farming was once primarily an activity to sustain the farmer and his family, farming can be a money-making enterprise. The two major avenues of income for a farm business are using the farm land and leasing the land to another farmer.'
+        title: 'Basic steps in cultivating coffee',
+        text:'Coffee plants are tropical evergreen shrubs that can be grown indoors or outdoors in warm climates. To plant coffee seeds, you need to prepare a rich, peat-based potting soil with good drainage and a slightly acidic pH12. You can sow one seed per container or use burlap sacks to sandwich the seeds34. You need to keep the seeds moist and shaded until they germinate, which may take several weeks53. Once the seedlings are strong enough, you can transplant them to larger pots or outdoor beds with partial shade and regular watering.'
     }
 ]
 
